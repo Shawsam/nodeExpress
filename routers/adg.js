943e2cd1f0d13dc904926521fe614d527b920d29
shawsam <1603334317@qq.com>
2018-05-15 16:49:17 +0800

    小程序wx.login,jscode换取openId

--- routers/adg.js
+++ routers/adg.js
@@ -13,12 +13,47 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 
+
+//接口-临时登录凭证code 获取 session_key 和 openid 等  方式-post 参数-jsCode
+router.get('/getOpenId', function(api_req, api_res) {
+    console.log('/***********************登录凭证code换取openId************************/')
+    console.log('请求参数，'+ JSON.stringify(api_req.query))
+    const JSCODE = api_req.query.jsCode
+    // console.log('请求参数，'+ JSON.stringify(api_req.body))
+    // const JSCODE = api_req.body.jsCode
+    const APPID = 'wxb8b8866dcb2f08b8'
+    const SECRET = '3d8e9c3c03f0141bad94765b0b3f2c5b'
+    const originUrl = `https://api.weixin.qq.com/sns/jscode2session?appid=${APPID}&secret=${SECRET}&js_code=${JSCODE}&grant_type=authorization_code`
+    superagent.get(originUrl)
+      .end(function (err, res) {
+         // 常规的错误处理
+         if (err) {
+             console.log(err)
+         }else{
+             var resText = JSON.parse(res.text)
+             if(resText.openid){
+                 resData.msg = '成功'
+                 resData.data =  resText
+                 console.log('处理结果，'+resData.msg)
+                 api_res.json(resData)
+             }else{
+                 resData.msg = '无效的jsCode'
+                 console.log('处理结果，'+resData.msg)
+                 api_res.json(resData)
+             }
+
+          }
+       })  
+})
+
+https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
+
 //接口-首页  方式-get 参数-page(页码)
 router.get('/indexData', function(api_req, api_res) {
     console.log('/************************访问主页商品************************/')
     console.log('请求参数，'+ JSON.stringify(api_req.query))
-    var pageNum = api_req.query.page||0
-    var jsonData = {"category":"","sort":"topSelling","size":"30","page":pageNum,"text":"","within":"","query":"allCategories:9","condition":{"brand":[],"gender":[],"priceRange":[0,5150],"specialPriceYnOption":false,"giftYnOption":false,"isNewProductOption":false,"fastShopYnOption":false,"flatPriceYnOption":false}}
+    const pageNum = api_req.query.page||0
+    const jsonData = {"category":"","sort":"topSelling","size":"30","page":pageNum,"text":"","within":"","query":"allCategories:9","condition":{"brand":[],"gender":[],"priceRange":[0,5150],"specialPriceYnOption":false,"giftYnOption":false,"isNewProductOption":false,"fastShopYnOption":false,"flatPriceYnOption":false}}
     const originUrl = 'http://www.shilladfs.com/estore/kr/zh/ajaxProducts'
 
     superagent.post(originUrl)
@@ -55,8 +90,8 @@ router.get('/indexData', function(api_req, api_res) {
 router.get('/productList', function(api_req, api_res) {
     console.log('/************************访问商品列表************************/')
     console.log('请求参数，'+ JSON.stringify(api_req.query))
-    var cate = api_req.query.cate||1
-    var data = { "form":"normal","sc":cate,"type":"topSelling","searchWord":"","rank":0 }
+    const cate = api_req.query.cate||1
+    const data = { "form":"normal","sc":cate,"type":"topSelling","searchWord":"","rank":0 }
     const originUrl = 'http://www.shilladfs.com/estore/kr/zh/e/bestshop/getBestProductList'
 
     superagent.post(originUrl)
