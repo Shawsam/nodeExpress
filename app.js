09b370effe9d0e42f33aafc1fbc3a578fff974c4
shawsam <1603334317@qq.com>
2019-05-16 17:48:06 +0800

    不设置跨域

--- app.js
+++ app.js
@@ -50,14 +50,14 @@ app.use(express.static('public'));
 app.use('/public',express.static('public'));
 
 //设置跨域访问  
-app.all('*', function(req, res, next) {  
-    res.header("Access-Control-Allow-Origin", "*");  
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");  
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
-    res.header("X-Powered-By",' 3.2.1')  
-    res.header("Content-Type", "application/json;charset=utf-8");  
-    next();  
-});  
+// app.all('*', function(req, res, next) {  
+//     res.header("Access-Control-Allow-Origin", "*");  
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+//     res.header("X-Powered-By",' 3.2.1')  
+//     res.header("Content-Type", "application/json;charset=utf-8");  
+//     next();  
+// });  
 
 //======== cookies配置 =========================//
 var User = require('./models/User.js');
